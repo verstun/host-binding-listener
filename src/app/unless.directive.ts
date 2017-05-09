cd098014b004ee5b9269f01e6b944d5d2bb7df27
verstun <svetazavolokina81@gmail.com>
2017-05-09 14:33:27 -0400

    unless directive

---
+++ src/app/unless.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  // name must be the same as agit  selector
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      //create element in container
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      //remove element from the DOM
+      this.vcRef.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
